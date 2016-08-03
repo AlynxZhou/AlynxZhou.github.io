2c6f8abaf69d80be9a89a89045e75b091a978ecd
S-X-ShaX <sxshax@gmail.com>
2016-08-03 18:16:59 +0800

    jsjs~

---
+++ assets/js/animate.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    var delay = 1;
+    var DELAY_STEP = 200;
+    var animationOptions = {opacity:1,top:0};
+
+    function animateMain() {
+        var dfd = $.Deferred();
+        var els = $('.animate-init');
+        var size = els.size();
+
+        els.each(function(index,el) {
+            delay++;
+            $(el).delay(index * DELAY_STEP).animate(animationOptions);
+            (size - 1 === index) && dfd.resolve();
+	    });
+
+        return dfd.promise();
+	}
+
+	animateMain();
+});
