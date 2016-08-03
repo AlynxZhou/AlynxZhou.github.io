9950277737d6fb6359923bc44a25c16797792151
S-X-ShaX <sxshax@gmail.com>
2016-08-03 17:59:48 +0800

    Changed push.sh.

--- push.sh
+++ push.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#-*- coding: utf-8 -*-
+
+### Filename: push.sh
+## Created by 请叫我喵 | S-X-ShaX
+# sxshax@gmail.com, http://sxshax.xyz/
 
 git add .
-git commit -m "Website updated."
 
-git push origin master
-git push coding master
+if [[ -n "${*}" ]]; then
+    git commit -m "${*}"
+else
+    git commit -m "Website updated."
+fi
+
+git push -u origin master
+git push -u coding master
