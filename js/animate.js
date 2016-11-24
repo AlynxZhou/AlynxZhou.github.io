14aee9f1ea21e95be1b9738b9a30c04f090bfa2b
S-X-ShaX <sxshax@gmail.com>
2016-11-24 11:43:13 +0800

    Changed jQuery and animate.js.

--- js/animate.js
+++ js/animate.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
     var delay = 1;
     var DELAY_STEP = 200;
-    var animationOptions = {opacity:1, top:0};
+    var animationOptions = {
+        "opacity": 1,
+        "top": 0
+    };
 
     function animateMain() {
         var dfd = $.Deferred();
-        var els = $('.animate-init');
-        var size = els.size();
+        var els = $(".animate-init");
+        var length = els.length;
 
         els.each(function(index, el) {
             delay++;
             $(el).delay(index * DELAY_STEP).animate(animationOptions);
-            (size - 1 === index) && dfd.resolve();
+            (length - 1 === index) && dfd.resolve();
         });
 
         return dfd.promise();
