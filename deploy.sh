c6a121476b8acd16775551b60f3bdc53a9fd2e38
Alynx Zhou <alynx.zhou@gmail.com>
2022-12-10 14:07:35 +0800

    Updated site.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-#-*- coding: utf-8 -*-
 
-# Filename: push.sh
-# Created by AlynxZhou
-# alynx.zhou@gmail.com, http://alynx.xyz/
-
-git add .
-
+git add --all
 if [[ -n "${*}" ]]; then
-    git commit -m "${*}"
+    git commit --message "${*}"
 else
-    git commit -m "Website updated."
+    git commit --message "Updated site."
 fi
-
-git push -u origin master
+git push --set-upstream origin master
 
 #rsync -azv --exclude=.git --exclude=.gitignore --exclude=.nojekyll --delete . alynx@172.105.212.166:~/alynx.xyz/
